1d0af505d6dfa95356fb12803294a940841d7a2d
devggui <devguiet@gmail.com>
2023-05-22 11:23:30 -0300

    feat: theme 1 finished

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+const theme1 = document.getElementById('theme1')
+const theme2 = document.getElementById('theme2')
+const theme3 = document.getElementById('theme3')
+
+theme1.addEventListener('change', toggleTheme)
+theme2.addEventListener('change', toggleTheme)
+theme3.addEventListener('change', toggleTheme)
+
+function toggleTheme() {
+  if(theme1.checked) {
+    console.log('tema 1')
+  } else if(theme2.checked) {
+    console.log('tema 2')
+  } else {
+    console.log('tema 3')
+  }
+}
+
 let result = document.getElementById('result');
 
 function appendCharacter(character) {
